c5d4f05709046923bc60ceb83327d13a3c91540d
80hdGuy <rihards.j98@gmail.com>
2022-07-07 08:50:30 +0300

    formatting fix

--- Loops/06.php
+++ Loops/06.php
@@ -1,16 +1,16 @@
 <?php
-const size = 7;
-const rowLength = (size - 1) * 4 * 2;
+$size = 7;
+$rowLength = ($size - 1) * 4 * 2;
 
-for ($i = 0; $i < size; $i++){
+for ($i = 0; $i < $size; $i++) {
     $halfStarTextSize = ($i * 4);
-    for ($j = 0; $j < rowLength / 2  - $halfStarTextSize; $j++){
+    for ($j = 0; $j < $rowLength / 2 - $halfStarTextSize; $j++) {
         echo "/";
     }
-    for ($j = 0; $j < $halfStarTextSize * 2; $j++){
+    for ($j = 0; $j < $halfStarTextSize * 2; $j++) {
         echo "*";
     }
-    for ($j = 0; $j < rowLength / 2 - $halfStarTextSize; $j++){
+    for ($j = 0; $j < $rowLength / 2 - $halfStarTextSize; $j++) {
         echo "\\";
     }
     echo "\n";
